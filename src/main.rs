3fb86cd4912ab03af1c08c7d25fb16722d731815
enpotid <xian101221@gmail.com>
2026-01-14 23:20:30 +0900

    change error msgs to TODOs

--- src/main.rs
+++ src/main.rs
@@ -7,16 +7,17 @@ use std::{env, fs, io::Read};
 fn main() {
     let args: Vec<String> = env::args().collect();
     if args.len() != 2 {
-        println!("example: nap main.nap");
-        return;
+        todo!(); // invalid arguments
     }
 
     let mut contents = String::new();
-    if let Ok(mut file) = fs::File::open(&args[1]) {
-        file.read_to_string(&mut contents).unwrap(); // edit unwrap later
-    } else {
-        println!("enter the correct path");
-        return;
+    match fs::File::open(&args[1]) {
+        Ok(mut file) => {
+            file.read_to_string(&mut contents).unwrap(); // edit unwrap later
+        }
+        Err(_) => {
+            todo!() // file open failed
+        }
     }
 
     let tokens = lexer::tokenize(&contents);
